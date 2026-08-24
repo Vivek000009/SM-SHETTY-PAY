76f869c3de9023cf671c226070b8e8564ddd59d4
Vivek000009 <vy4767132@gmail.com>
2026-08-24 21:49:03 +0530

    Save PWA icon links

--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -1,17 +1,21 @@
-const CACHE_NAME = "sm-shetty-pay-v1";
+const CACHE_NAME = "sm-shetty-pay-v2";
 
 const APP_FILES = [
     "/SM-SHETTY-PAY/",
     "/SM-SHETTY-PAY/index.html",
     "/SM-SHETTY-PAY/style.css",
     "/SM-SHETTY-PAY/app.js",
-    "/SM-SHETTY-PAY/manifest.json"
+    "/SM-SHETTY-PAY/manifest.json",
+    "/SM-SHETTY-PAY/icon-192.png",
+    "/SM-SHETTY-PAY/icon-512.png"
 ];
 
 
 self.addEventListener(
     "install",
     event => {
+
+        self.skipWaiting();
 
         event.waitUntil(
 
@@ -33,25 +37,31 @@ self.addEventListener(
 
         event.waitUntil(
 
-            caches
-                .keys()
-                .then(keys => {
+            Promise.all([
 
-                    return Promise.all(
+                caches
+                    .keys()
+                    .then(keys => {
 
-                        keys
-                            .filter(
-                                key =>
-                                    key !== CACHE_NAME
-                            )
-                            .map(
-                                key =>
-                                    caches.delete(key)
-                            )
+                        return Promise.all(
 
-                    );
+                            keys
+                                .filter(
+                                    key =>
+                                        key !== CACHE_NAME
+                                )
+                                .map(
+                                    key =>
+                                        caches.delete(key)
+                                )
 
-                })
+                        );
+
+                    }),
+
+                self.clients.claim()
+
+            ])
 
         );
 
@@ -65,13 +75,28 @@ self.addEventListener(
 
         event.respondWith(
 
-            caches
-                .match(event.request)
-                .then(cachedResponse => {
+            fetch(event.request)
+                .then(response => {
 
-                    return (
-                        cachedResponse ||
-                        fetch(event.request)
+                    const copy =
+                        response.clone();
+
+                    caches
+                        .open(CACHE_NAME)
+                        .then(cache => {
+                            cache.put(
+                                event.request,
+                                copy
+                            );
+                        });
+
+                    return response;
+
+                })
+                .catch(() => {
+
+                    return caches.match(
+                        event.request
                     );
 
                 })
